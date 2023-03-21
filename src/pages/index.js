717a1325d6376e9eb628d5bca4a62fd4541f7f03
tszchung019 <44861788+tszchung019@users.noreply.github.com>
2023-03-21 12:01:44 +0000

    added-background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const Home = () => {
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>Welcome to GeeksforGeeks</h1>
-	</div>
-);
+	return (
+		<body>
+			<div>
+				<section>
+					<h2>WWF History</h2>
+					<p>The World Wide Fund for Nature (WWF) is an international organization working on issues regarding the conservation, research and restoration of the environment, formerly named the World Wildlife Fund. WWF was founded in 1961.</p>
+				</section>
+				<section>
+					<h2>WWF's Symbol</h2>
+					<p>The Panda has become the symbol of WWF. The well-known panda logo of WWF originated from a panda named Chi Chi that was transferred from the Beijing Zoo to the London Zoo in the same year of the establishment of WWF.</p>
+				</section>
+			</div>
+		</body>
+	);
 };
 
 export default Home;
