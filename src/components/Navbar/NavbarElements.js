4928e02e5910ffd7314f879a53a0d0eac175a205
tszchung019 <44861788+tszchung019@users.noreply.github.com>
2023-03-22 18:32:41 +0000

    background updated

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);
+background: linear-gradient(225deg, #74EBD5 0%, #62BCD6 100%);
 height: 85px;
 display: flex;
 justify-content: space-between;
