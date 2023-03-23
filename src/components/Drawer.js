78d073b2b957a93774480b183bee5ed2698944db
tszchung019 <44861788+tszchung019@users.noreply.github.com>
2023-03-23 16:34:28 +0000

    Navbar design updated

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -4,9 +4,17 @@ import {
   IconButton,
   List,
   ListItem,
-  ListItemText
+  ListItemText,
+  ListItemButton
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import EventIcon from '@mui/icons-material/Event';
+import FolderIcon from '@mui/icons-material/Folder';
+import WorkIcon from '@mui/icons-material/Work';
+import ArticleIcon from '@mui/icons-material/Article';
+import PeopleIcon from '@mui/icons-material/People';
 import { Link } from "react-router-dom";
 import "../css/styles.css"
 
@@ -19,50 +27,57 @@ return (
         onClose={() => setOpenDrawer(false)}
         PaperProps={{
             sx: {
-              backgroundColor: "#3f99bf",
+              backgroundColor: "#045572",
               width: "250px"
             },
           }}
     >
         <List>
         <ListItem onClick={() => setOpenDrawer(false)}>
+            <HomeIcon className="navBarIcon"/>
             <ListItemText>
             <Link className="navBarItem" to="/">Home</Link>
             </ListItemText>
         </ListItem>
         <ListItem onClick={() => setOpenDrawer(false)}>
+            <InfoIcon className="navBarIcon"/>
             <ListItemText>
             <Link className="navBarItem" to="/about">About</Link>
             </ListItemText>
         </ListItem>
         <ListItem onClick={() => setOpenDrawer(false)}>
+            <EventIcon className="navBarIcon"/>
             <ListItemText>
             <Link className="navBarItem" to="/events">Events</Link>
             </ListItemText>
         </ListItem>
         <ListItem onClick={() => setOpenDrawer(false)}>
+            <FolderIcon className="navBarIcon"/>
             <ListItemText>
             <Link className="navBarItem" to="/projects">Projects</Link>
             </ListItemText>
         </ListItem>
         <ListItem onClick={() => setOpenDrawer(false)}>
+            <WorkIcon className="navBarIcon"/>
             <ListItemText>
             <Link className="navBarItem" to="/work">Services</Link>
             </ListItemText>
         </ListItem>
         <ListItem onClick={() => setOpenDrawer(false)}>
+            <ArticleIcon className="navBarIcon"/>
             <ListItemText>
             <Link className="navBarItem" to="/blogs">Blogs</Link>
             </ListItemText>
         </ListItem>
-        <ListItem onClick={() => setOpenDrawer(false)}>
-            <ListItemText>
-            <Link className="navBarItem" to="/signup">Sign Up / Log In</Link>
-            </ListItemText>
+        <ListItem id="signupButton" onClick={() => setOpenDrawer(false)}>
+            <PeopleIcon className="navBarIcon"/>
+            <ListItemButton>
+            <Link className="navBarButton" to="/signup">Sign Up / Log In</Link>
+            </ListItemButton>
         </ListItem>
         </List>
     </Drawer>
-    <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+    <IconButton id="menuIcon" onClick={() => setOpenDrawer(!openDrawer)}>
         <MenuIcon />
     </IconButton>
     </>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -15,7 +15,7 @@ export default function App() {
             <div class="content">
                 <h1>Hello {user.username}</h1>
                 <section>
-                    <p>This is your portal for managing your projects with me. You can start a new project, asking for a quote, and manage the work schedule with existing projects</p>
+                    <p>This is your portal for managing your projects with me. You can start a new project, ask for a quote, and manage the work schedule with existing projects</p>
                 </section>
                 <button onClick={signOut}>Sign out</button>
             </div>
