f276017fbd5e138534a40ef4a22ed1ba31adce96
tszchung019 <44861788+tszchung019@users.noreply.github.com>
2023-04-03 12:27:55 +0100

    get posts function updated

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,112 +1,138 @@
 import React, { useState, useEffect } from "react";
-import '../css/styles.css';
+import "../css/styles.css";
 import { API } from "aws-amplify";
 import {
-	Button,
-	Flex,
-	Heading,
-	Text,
-	TextField,
-	View,
-  } from "@aws-amplify/ui-react";
+  Button,
+  Flex,
+  Heading,
+  Text,
+  TextField,
+  View,
+  Link,
+} from "@aws-amplify/ui-react";
 import { 
-	getBlog, 
-	listBlogs, 
-	getPost, 
-	listPosts, 
-	getComment, 
-	listComments 
+  getBlog as getBlogQuery, 
+  listBlogs as listBlogsQuery, 
+  getPost, 
+  listPosts, 
+  getComment, 
+  listComments 
 } from "../graphql/queries"
 import {
-	createBlog as createBlogMutation,
-	updateBlog as updateBlogMutation,
-	deleteBlog as deleteBlogMutation,
-	createPost as createPostMutation,
-	updatePost as updatePostMutation,
-	deletePost as deletePostMutation,
-	createComment as createCommentMutation,
-	updateComment as updateCommentMutation,
-	deleteComment as deleteCommentMutation,
+  createBlog as createBlogMutation,
+  updateBlog as updateBlogMutation,
+  deleteBlog as deleteBlogMutation,
+  createPost as createPostMutation,
+  updatePost as updatePostMutation,
+  deletePost as deletePostMutation,
+  createComment as createCommentMutation,
+  updateComment as updateCommentMutation,
+  deleteComment as deleteCommentMutation,
 } from "../graphql/mutations"
 
 const Blogs = () => {
-	const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-	useEffect(() => {
-		fetchBlogs();
-	}, []);
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
 
-	async function fetchBlogs() {
-		const apiData = await API.graphql({ query: listBlogs });
-		const blogsFromAPI = apiData.data.listBlogs.items;
-		setBlogs(blogsFromAPI);
-	}
+  async function fetchBlogs() {
+    const apiData = await API.graphql({ query: listBlogsQuery });
+    const blogsFromAPI = apiData.data.listBlogs.items;
+    setBlogs(blogsFromAPI);
+  }
 
-	async function createBlog(event) {
-		event.preventDefault();
-		const form = new FormData(event.target);
-		const data = {
-			name: form.get("name"),
-		};
-		await API.graphql({
-			query: createBlogMutation,
-			variables: { input: data },
-		});
-		fetchBlogs();
-		event.target.reset();
-	}
+  async function createBlog(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const data = {
+      name: form.get("name"),
+    };
+    await API.graphql({
+      query: createBlogMutation,
+      variables: { input: data },
+    });
+    fetchBlogs();
+    event.target.reset();
+  }
 
-	async function deleteBlog({ id }) {
-		const newBlogs = blogs.filter((blog) => blog.id !== id);
-		console.log(typeof(id));
-		setBlogs(newBlogs);
-		await API.graphql({
-			query: deleteBlogMutation,
-			variables: { input: { id } },
-		});
-	}
+  async function deleteBlog({ id }) {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+    await API.graphql({
+      query: deleteBlogMutation,
+      variables: { input: { id } },
+    });
+  }
 
-	return (
-		<div class="content">
-		<h1>Welcome to Arthur's Blogs</h1>
-		<View className="Blog">
-			<Heading level={1}>My Blogs</Heading>
-			<View as="form" margin="3rem 0" onSubmit={createBlog}>
-				<Flex direction="row" justifyContent="center">
-				<TextField
-					name="name"
-					placeholder="Blog Name"
-					label="Blog Name"
-					labelHidden
-					variation="quiet"
-					required
-				/>
-				<Button type="submit" variation="primary">
-					Create Blog
-				</Button>
-				</Flex>
-			</View>
-			<Heading level={2}>Current Blogs</Heading>
-			<View margin="3rem 0">
-				{blogs.map((blog) => (
-				<Flex
-					key={blog.id || blog.name}
-					direction="row"
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Text as="strong" fontWeight={700}>
-					{blog.name}
-					</Text>
-					<Button variation="link" onClick={() => deleteBlog(blog)}>
-					Delete blog
-					</Button>
-				</Flex>
-				))}
-			</View>
-			</View>
-		</div>
-	);
+  async function getPostsfromBlog({ id }) {
+    const apiData = await API.graphql({
+      query: getBlogQuery,
+      variables: { id: id },
+    });
+    const postsFromAPI = apiData.data.getBlog.posts.items;
+    setPosts(postsFromAPI);
+  }
+
+  return (
+    <div class="content">
+      <h1>Welcome to Arthur's Blogs</h1>
+      <View className="Blog">
+        <Heading level={1}>My Blogs</Heading>
+        <View as="form" margin="3rem 0" onSubmit={createBlog}>
+          <Flex direction="row" justifyContent="center">
+            <TextField
+              name="name"
+              placeholder="Blog Name"
+              label="Blog Name"
+              labelHidden
+              variation="quiet"
+              required
+            />
+            <Button type="submit" variation="primary">
+              Create Blog
+            </Button>
+          </Flex>
+        </View>
+        <Heading level={2}>Current Blogs</Heading>
+        <View margin="3rem 0">
+          {blogs.map((blog) => (
+            <Flex
+              key={blog.id || blog.name}
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Link
+                onClick={() => {
+                  getPostsfromBlog(blog);
+                }}
+                as="strong"
+                fontWeight={700}
+              >
+                {blog.name}
+              </Link>
+
+              <Button variation="link" onClick={() => deleteBlog(blog)}>
+                Delete blog
+              </Button>
+            </Flex>
+          ))}
+        </View>
+        <Heading level={2}>Posts</Heading>
+        <View>
+          {posts.map((post) => (
+            <View key={post.id}>
+              <Heading level={3}>{post.title}</Heading>
+              <Text>{post.content}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </div>
+  );
 };
 
 export default Blogs;
